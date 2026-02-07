da70babb9e707763162f62711ff2283a2ccc252e
Dramzies <ramismail701@gmail.com>
2026-02-07 17:47:13 +0300

    Update codebase

--- server.js
+++ server.js
@@ -333,6 +333,5 @@ app.delete('/api/admin/appointments/:id', checkAdminSession, (req, res) => {
 // 5. START SERVER
 // ======================================
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
-    console.log(`Available at https://dervistas-thepeninssula-salon.onrender.com`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
